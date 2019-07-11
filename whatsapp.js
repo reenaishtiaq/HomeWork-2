3aaa1e42a7d0184f5f7bc377b65d99a2388ea7ba
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-11 15:24:42 +0500

    Add files via upload

---
+++ whatsapp.js
@@ -0,0 +1,34 @@
+// Data of Whatsapp
+
+// used array
+
+let contactName = [
+    "Arslan", "Abbas", "Abdul", "Adeeba", "Adeel",
+    "Adnan", "Afzal", "Babar", "Rabia", "Barzana", "Bashar", "Bashir", " Behram",
+    "Dalir", "Mehwish", "Tabassum", "Rahila",
+    "Ejaz", "Anooshy", "Zartasha",
+    "Falak", "Fatima", "Ambereen", "Andaleeb.", "Aminah", "Farrokh",
+    "Yasmin", "Lubna", "Parisa", "Gohar", "Saima",
+    "Komal", "Maryam", "Iman", "Mawra", "Sahar",
+    "Namra", "Tahmina", "Ismail", "Urooj", "Rayyan", "Ilyas"
+]
+
+let contactNumbers = [
+    "0312-1234567",
+    "0312-1234567",
+    "0312-1234567",
+    "0312-1234567",
+    "0312-1234567",
+    "0312-1234567",
+    "0312-1234567",
+    "0312-1234567",
+    "0312-1234567",
+    "0312-1234567"
+]
+
+let messages = [
+    msgOfMaryam = "Hi, i'm Maryam. How was your day",
+    msgOfAnooshy = "I'm busy now!",
+    msgOfArslan = "Come back to home",
+    msgOfUrooj = "There is no class today"
+]
